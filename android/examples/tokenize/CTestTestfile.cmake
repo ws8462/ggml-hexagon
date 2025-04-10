033331cf592de68750eb100d6788fa0baf9cf8b1
ws8462 <ws8462@naver.com>
2025-04-10 10:59:11 +0900

    android

---
+++ android/examples/tokenize/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kws/ggml-hexagon/examples/tokenize
+# Build directory: /home/kws/ggml-hexagon/build-android/examples/tokenize
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
