6dd6f05a39a45cdf694b1f1ea99d95f09807c13b
ws8462 <ws8462@naver.com>
2025-05-15 16:04:32 +0900

    a

--- android/llama-version.cmake
+++ android/llama-version.cmake
@@ -9,19 +9,19 @@
 # The variable CVF_VERSION must be set before calling configure_file().
 
 
-set(PACKAGE_VERSION "0.0.5198")
+set(PACKAGE_VERSION "0.0.5199")
 
 if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
   set(PACKAGE_VERSION_COMPATIBLE FALSE)
 else()
 
-  if("0.0.5198" MATCHES "^([0-9]+)\\.")
+  if("0.0.5199" MATCHES "^([0-9]+)\\.")
     set(CVF_VERSION_MAJOR "${CMAKE_MATCH_1}")
     if(NOT CVF_VERSION_MAJOR VERSION_EQUAL 0)
       string(REGEX REPLACE "^0+" "" CVF_VERSION_MAJOR "${CVF_VERSION_MAJOR}")
     endif()
   else()
-    set(CVF_VERSION_MAJOR "0.0.5198")
+    set(CVF_VERSION_MAJOR "0.0.5199")
   endif()
 
   if(PACKAGE_FIND_VERSION_RANGE)
