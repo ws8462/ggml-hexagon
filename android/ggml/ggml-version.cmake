066a99b2d618138665237bffd749e7b50cf93022
ws8462 <ws8462@naver.com>
2025-05-15 17:02:04 +0900

    aa

--- android/ggml/ggml-version.cmake
+++ android/ggml/ggml-version.cmake
@@ -9,19 +9,19 @@
 # The variable CVF_VERSION must be set before calling configure_file().
 
 
-set(PACKAGE_VERSION "0.0.5200")
+set(PACKAGE_VERSION "0.0.5201")
 
 if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
   set(PACKAGE_VERSION_COMPATIBLE FALSE)
 else()
 
-  if("0.0.5200" MATCHES "^([0-9]+)\\.")
+  if("0.0.5201" MATCHES "^([0-9]+)\\.")
     set(CVF_VERSION_MAJOR "${CMAKE_MATCH_1}")
     if(NOT CVF_VERSION_MAJOR VERSION_EQUAL 0)
       string(REGEX REPLACE "^0+" "" CVF_VERSION_MAJOR "${CVF_VERSION_MAJOR}")
     endif()
   else()
-    set(CVF_VERSION_MAJOR "0.0.5200")
+    set(CVF_VERSION_MAJOR "0.0.5201")
   endif()
 
   if(PACKAGE_FIND_VERSION_RANGE)
