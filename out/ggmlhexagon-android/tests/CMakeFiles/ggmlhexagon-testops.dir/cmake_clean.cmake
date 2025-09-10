66c5800b9cdbdb9f8eab8243b8f92b1911cc4df7
ws8462 <ws8462@naver.com>
2025-09-10 17:32:01 +0900

    a

---
+++ out/ggmlhexagon-android/tests/CMakeFiles/ggmlhexagon-testops.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/ggmlhexagon-testops"
+  "../bin/ggmlhexagon-testops.pdb"
+  "CMakeFiles/ggmlhexagon-testops.dir/get-model.cpp.o"
+  "CMakeFiles/ggmlhexagon-testops.dir/get-model.cpp.o.d"
+  "CMakeFiles/ggmlhexagon-testops.dir/ggmlhexagon-testops.cpp.o"
+  "CMakeFiles/ggmlhexagon-testops.dir/ggmlhexagon-testops.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ggmlhexagon-testops.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
